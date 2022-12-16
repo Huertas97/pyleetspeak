9835240802d5eda1287120e9e4c679171665938a
Huertas97 <ahuertasg01@gmail.com>
2022-12-16 11:07:46 +0000

    Add Augmenter to import path

--- pyleetspeak/__init__.py
+++ pyleetspeak/__init__.py
@@ -7,3 +7,4 @@ from .format_converter import to_bilou_and_iob_format
 from .Leet_NER_generator import NER_data_generator
 from .modes import *
 from .spacy_ner_formal_test import spacy_formal_test, plot_confusion_matrix
+from .WordCamouflage_Augmenter import augmenter
